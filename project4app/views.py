5646c8b897f02bf3c0aac1f22d82c9a7d699391d
vijayvignesh021 <vijayvickey21@gmail.com>
2023-02-04 20:28:13 +0530

    new

--- project4app/views.py
+++ project4app/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import HttpResponse,render
+import geocoder
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim(user_agent="geoapiExercises")
  
 
 def home(request) :
- 
-    return render(request,'project4app/index2.html')
+    adr = geocoder.ip('me')
+    latitude = adr.latlng[0]
+    longitude =  adr.latlng[1]
+    city = get_city(latitude, longitude)
+    location  = city
+    return render(request,'project4app/index2.html',{'loc':location})
+
+def get_city(latitude, longitude):
+    location = geolocator.reverse(f"{latitude}, {longitude}", exactly_one=True)
+    return location.raw['address'].get('town', location.raw['address'].get('city', ''))
