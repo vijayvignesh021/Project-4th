db566e93c313e4ed46a423dfb426104b9c925eb7
vijayvignesh021 <vijayvickey21@gmail.com>
2023-02-05 20:33:33 +0530

    1st temp

--- project4app/urls.py
+++ project4app/urls.py
@@ -3,4 +3,6 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('track/', views.track, name='track'),
 ]
--- project4app/views.py
+++ project4app/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import HttpResponse,render
-import geocoder
-from geopy.geocoders import Nominatim
+from django.http import JsonResponse
 
-geolocator = Nominatim(user_agent="geoapiExercises")
  
 
 def home(request) :
-    adr = geocoder.ip('me')
-    latitude = adr.latlng[0]
-    longitude =  adr.latlng[1]
-    city = get_city(latitude, longitude)
-    location  = city
-    return render(request,'project4app/index2.html',{'loc':location})
+    # Process data in Python
+    return render(request,'project4app/index.html')
 
-def get_city(latitude, longitude):
-    location = geolocator.reverse(f"{latitude}, {longitude}", exactly_one=True)
-    return location.raw['address'].get('town', location.raw['address'].get('city', ''))
+def login(request) :
+    # Process data in Python
+    return render(request,'project4app/login.html')
+
+def track(request) :
+    # Process data in Python
+    return render(request,'project4app/track.html')
+
